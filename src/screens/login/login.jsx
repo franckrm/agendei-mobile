3e4f625dae5360bc4f5d15a824f693f9a98964ce
3º Sgt Ribeiro <franckrm2001@gmail.com>
2024-11-14 18:43:42 -0300

    wip: creating login view

---
+++ src/screens/login/login.jsx
@@ -0,0 +1,10 @@
+import { Image, Text, View } from "react-native";
+import logo from "../../assets/logo.png"
+
+function Login(){
+    return <View>
+        <Image source={logo}  style={ } />
+    </View>
+}
+
+export default Login;
---
+++ src/screens/login/login.style.js
@@ -0,0 +1,9 @@
+import {COLORS, FONT_SIZE} from "../../constants/theme"
+
+export const styles = {
+    
+    logo:{
+        width:
+    }
+    
+}
